37f5e190c6f82ab5277894fa7c1cdc3414916ba5
nbplus <plusnb88@gmail.com>
2026-09-24 14:01:24 +0800

    1

---
+++ linkedlist/single_linkedlist.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// 输入一个数组，转换为一条单链表
+ListNode* createLinkedList(const std::vector<int>& arr) {
+    if (arr.empty()) {
+        return nullptr;
+    }
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* cur = head;
+    for (int i = 1; i < arr.size(); i++) {
+        cur->next = new ListNode(arr[i]);
+        cur = cur->next;
+    }
+    return head;
+}
+
